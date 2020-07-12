b6d1e9a998e41518864ac3cfc51bdf998a6e9b30
taschunina <taschunina@yandex.ru>
2020-07-12 14:47:11 +0300

    Add to webpack image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          "file-loader?name=../images/[name].[ext]", // указали папку, куда складывать изображения
+          {
+            loader: "image-webpack-loader",
+            options: {},
+          },
+        ],
+      },
     ],
   },
   plugins: [
