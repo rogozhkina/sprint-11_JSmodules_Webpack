0c4c031b57bbe2f950126375010df8ab551742eb
taschunina <taschunina@yandex.ru>
2020-07-12 13:26:37 +0300

    Add to webpack.config rules for js

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,14 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  module: {
+    rules: [
+      {
+        // тут описываются правила
+        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+        exclude: /node_modules/, // исключает папку node_modules
+      },
+    ],
+  },
 };
