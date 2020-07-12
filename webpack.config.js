117a463eeb00a304872e1517501dfe9cbbb6b2b4
taschunina <taschunina@yandex.ru>
2020-07-12 13:58:18 +0300

    Add to webpack html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -27,6 +28,13 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "style.css",
+    }),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      hash: true, // для страницы нужно считать хеш
+      template: "./src/index.html", // откуда брать образец для сравнения с текущим видом проекта
+      filename: "index.html", // имя выходного файла, то есть того, что окажется в папке dist после сборки
     }),
   ],
 };
