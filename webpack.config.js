c6dd8466ca1a0d961751a1472bdf96dd66af7566
taschunina <taschunina@yandex.ru>
2020-07-12 14:02:44 +0300

    Add hash-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
 
 module.exports = {
   entry: {
